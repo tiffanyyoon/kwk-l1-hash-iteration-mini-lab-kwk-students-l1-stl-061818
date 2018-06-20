6436eeb6cc9d6c5bed4894e55f13a2dfd11aafe4
tiffanyyoon <yyoon032@rsdmo.org>
2018-06-20 16:46:26 +0000

    Done.

---
+++ hash.rb
@@ -0,0 +1,12 @@
+summer_olympics = {
+  "Atlanta" => 1996,
+  "Sydney" => 2000,
+  "Athens" => 2004,
+  "Beijing" => 2008,
+  "London" => 2012
+}
+
+summer_olympics.each do |place, year|
+  puts "The #{year} summer olympics took place in #{place}."
+  puts ""
+end
